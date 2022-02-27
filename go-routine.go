84b979c097f32e7de7f08bac451f2a7cba738de0
alfiancikoa <sikaraeng@gmail.com>
2022-02-27 18:34:47 +0800

    go routine

--- go-routine.go
+++ go-routine.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func Says(str string) {
+	for i := 0; i < 5; i++ {
+		// Delay 100 ms
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(str)
+	}
+}
 
 func main() {
 	fmt.Println("Hello World")
+	go Says("world")
+	Says("Hello")
+	/*
+		Hello World
+		world
+		Hello
+		Hello
+		world
+		world
+		Hello
+		Hello
+		world
+		Hello
+	*/
 }
